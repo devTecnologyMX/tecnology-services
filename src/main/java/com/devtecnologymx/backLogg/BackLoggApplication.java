f557ca00ae45f85fe91b5555c0fb99c3d5c61bcf
José Antonio García Lara <14030365@itesa.edu.mx>
2023-12-27 22:29:39 -0600

    MI PRIMER COMMIT PARA DUMMIES

---
+++ src/main/java/com/devtecnologymx/backLogg/BackLoggApplication.java
@@ -0,0 +1,14 @@
+package com.devtecnologymx.backLogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLoggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLoggApplication.class, args);
+		System.out.println("HOLA");
+	}
+
+}
---
+++ src/test/java/com/devtecnologymx/backLogg/BackLoggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtecnologymx.backLogg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLoggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
